369e35161bbc2ca7c953ae730a3df1af6dcfdbe3
Rohadgal <gal.frias@gmail.com>
2024-09-17 01:23:51 -0600

    passCode appears in panel of room 3

--- Assets/Scripts/ArduinoController.cs
+++ Assets/Scripts/ArduinoController.cs
@@ -1,6 +1,4 @@
-using System;
 using System.IO.Ports;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ArduinoController : MonoBehaviour
--- Assets/Scripts/TriggerVerification.cs
+++ Assets/Scripts/TriggerVerification.cs
@@ -1,10 +1,15 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerVerification : MonoBehaviour{
     [SerializeField] private List<CubeTrigger> cubeTriggers;
+    [SerializeField] private GameObject codeImage;
     private bool canShowCode;
+
+    private void Start(){
+        if(codeImage != null){ codeImage.SetActive(false);}
+    }
+
     void Update(){
         int countCubes = 0;
         foreach (CubeTrigger cubeTrigger in cubeTriggers) {
@@ -14,6 +19,7 @@ public class TriggerVerification : MonoBehaviour{
 
             if (countCubes == 4 && !canShowCode) {
                 canShowCode = true;
+                codeImage?.SetActive(true);
                 Debug.Log("SHOW PASSCODE");
             }
         }
