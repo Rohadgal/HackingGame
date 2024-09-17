f4c7f9033beedd777c69e390d6dd8877c9e17c2e
Valentina Garcia <107201377+Vuvaluu@users.noreply.github.com>
2024-09-17 14:14:53 -0600

    Mensaje de interactuar terminado

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -1,26 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerActions : MonoBehaviour
 {
     public GameObject player;
     public Transform holdPos;
     private float throwForce = 500f; 
-    private float pickUpRange = 5f; 
-    private float rotationSensitivity = 1f; 
+    private float pickUpRange = 1f; 
     private GameObject heldObj; 
     private Rigidbody heldObjRb; 
     private bool canDrop = true; 
     private int LayerNumber; 
+    private bool messageVisible = false;
+    public Text messageText;
 
     void Start()
     {
-        LayerNumber = LayerMask.NameToLayer("holdLayer"); 
+        LayerNumber = LayerMask.NameToLayer("holdLayer");
+        if (messageText != null)
+        {
+            messageText.gameObject.SetActive(false);
+        }
     }
     void Update()
     {
         SelectObject();
+        CheckObjectInRange();
     }
 
     private void SelectObject()
@@ -32,7 +39,7 @@ public class PlayerActions : MonoBehaviour
                 RaycastHit hit;
                 if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
                 {
-                    if (hit.transform.gameObject.tag == "Cube")
+                    if (hit.transform.CompareTag("Cube"))
                     {
                         PickUpObject(hit.transform.gameObject);
                     }
@@ -104,4 +111,39 @@ public class PlayerActions : MonoBehaviour
             heldObj.transform.position = transform.position + new Vector3(0f, -0.5f, 0f);
         }
     }
+
+    void CheckObjectInRange()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
+        {
+            if (hit.transform.CompareTag("Cube"))
+            {
+                ShowMessage();
+            }
+        }
+        else
+        {
+            HideMessage();
+        }
+    }
+
+    void ShowMessage()
+    {
+        if(messageText != null && !messageVisible)
+        {
+            messageText.gameObject.SetActive(true);
+            messageVisible = true;
+        }
+    }
+
+    void HideMessage()
+    {
+        if (messageText != null && messageVisible)
+        {
+            messageText.gameObject.SetActive(false);
+            messageVisible = false;
+        }
+    }
+    
 }
